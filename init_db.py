0b2fcd2e66d0a68ea9afc435d081f37de991ca63
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-05-04 00:46:46 -0300

    Refactor database path extraction to use urlparse for improved clarity

--- init_db.py
+++ init_db.py
@@ -2,9 +2,14 @@ import os
 from database import Base, engine
 from seed.seed_users import seed_users
 from seed.seed_equipment import seed_equipment
+from urllib.parse import urlparse
 
 # Extrai o caminho do arquivo .db
-db_path = os.getenv("DB_PATH", "sqlite:///./academia.db").replace("sqlite:///", "")
+# db_path = os.getenv("DB_PATH", "sqlite:///./academia.db").replace("sqlite:///", "")
+
+raw_path = os.getenv("DB_PATH", "sqlite:///./academia.db")
+parsed = urlparse(raw_path)
+db_path = parsed.path
 
 if not os.path.exists(db_path):
     print("🔧 Criando o banco de dados e aplicando seeds...")
